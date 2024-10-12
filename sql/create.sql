cc3daea99f691df7044bd23caf3f8b38923d8754
chen <39870481+cpupg@users.noreply.github.com>
2024-10-12 23:47:37 +0800

    修改表名和模式名

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,13 @@
-create table user(
-    id int not null primary key,
-    username varchar(64) not null default  '',
-    age smallint not null default 18,
+create schema mybatis;
+
+create table mybatis.t_user
+(
+    id varchar(17) not null primary key,
+    username varchar(64) not null default '',
+    age int not null default 18,
+    birthday timestamp not null default current_timestamp(),
+    delete_status int not null default 0,
+    create_time timestamp not null default current_timestamp(),
+    update_time timestamp,
+    delete_time timestamp
 );
